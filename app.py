7922236bdfe6e7d8030611bc3cb067d26e44329f
danielpinto-developer <danielpinto.developer@gmail.com>
2025-07-20 00:39:01 -0600

    added site to cors middleware

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://www.beautyblossoms.net"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
